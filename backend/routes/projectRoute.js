60c74119837c0c367600ee04cf9543aee00dd30d
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-09-23 02:13:12 -0400

    get project

--- backend/routes/projectRoute.js
+++ backend/routes/projectRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { createProject, deleteProject, getProject } from "../controller/projectController.js";
+import { createProject, deleteProject, getProject, getProjects } from "../controller/projectController.js";
 
 
 const router = express.Router();
 
 router.post('/createproject', createProject);
-router.post('/getproject', getProject);
-router.post('/deleteproject', deleteProject)
+router.post('/getproject', getProjects);
+router.post('/deleteproject', deleteProject);
+router.post('/getpro', getProject);  // single project get;
 
 
 export default router;
