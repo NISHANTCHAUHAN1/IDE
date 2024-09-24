f0fadd9061a9ce81f16019ac68a95ced92fe2cc5
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-09-24 02:12:15 -0400

    contact done

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -7,58 +7,33 @@ const About = () => {
     <>
       <Navbar />
 
-      {/* <div className="container mx-auto min-h-screen w-screen flex flex-col lg:flex-row items-center justify-between px-4 lg:px-[100px]">
-        <div className="lg:w-1/2 w-full flex justify-center lg:justify-start">
+      <div className="container mx-auto min-h-screen w-screen flex flex-col lg:flex-row items-center justify-between px-4 lg:px-[50px]">
+        {/* Image Section */}
+        <div className="lg:w-[40%] w-full flex justify-center lg:justify-start">
           <img
-            className="object-cover rounded-full"
+            className="object-cover rounded-lg"
             src={image}
             alt="Nishaant Chauhan"
-            width="300" // Adjusted to smaller size for responsiveness
-            height="300"
+            width="400" // Adjusted width
+            height="400" // Adjusted height
           />
         </div>
 
+        {/* Text Section */}
         <div className="right lg:w-[55%] w-full text-center lg:text-left mt-8 lg:mt-0">
-          <h1 className="text-3xl lg:text-5xl font-bold text-gray-800">
-             Happay Coding❕
+          <h1 className="text-3xl lg:text-4xl font-bold text-gray-800">
+            Happy Coding❕
           </h1>
           <p className="text-lg lg:text-xl text-gray-600 mt-4">
             Hello, I am Nishant Chauhan from Delhi. You can use this code editor
             to create small projects using HTML, CSS, and JavaScript.
           </p>
           <h1 className="text-lg lg:text-xl text-gray-600 mt-4">
-            You can save your code or files using <span className="font-bold text-white">Ctrl + S</span>
+            You can save your code or files using{" "}
+            <span className="font-bold text-green-400">Ctrl + S</span>
           </h1>
         </div>
-      </div> */}
-
-<div className="container mx-auto min-h-screen w-screen flex flex-col lg:flex-row items-center justify-between px-4 lg:px-[50px]">
-  {/* Image Section */}
-  <div className="lg:w-[40%] w-full flex justify-center lg:justify-start">
-    <img
-      className="object-cover rounded-lg"
-      src={image}
-      alt="Nishaant Chauhan"
-      width="400" // Adjusted width
-      height="400" // Adjusted height
-    />
-  </div>
-
-  {/* Text Section */}
-  <div className="right lg:w-[55%] w-full text-center lg:text-left mt-8 lg:mt-0">
-    <h1 className="text-3xl lg:text-4xl font-bold text-gray-800">
-      Happy Coding❕
-    </h1>
-    <p className="text-lg lg:text-xl text-gray-600 mt-4">
-      Hello, I am Nishant Chauhan from Delhi. You can use this code editor
-      to create small projects using HTML, CSS, and JavaScript.
-    </p>
-    <h1 className="text-lg lg:text-xl text-gray-600 mt-4">
-      You can save your code or files using <span className="font-bold text-green-400">Ctrl + S</span>
-    </h1>
-  </div>
-</div>
-
+      </div>
     </>
   );
 };
--- frontend/src/utils/cn.js
+++
@@ -1,7 +0,0 @@
-// import { ClassValue,  } from "clsx";
-import {clsx} from "clsx"
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs) {
-  return twMerge(clsx(inputs));
-}
