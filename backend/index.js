8844b1439e6e67c621f4d9ea286e4cb6c6fd70d6
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-09-25 18:53:47 -0400

    update

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,17 @@ import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
 import cors from 'cors';
 import connectDb from './Database/db.js';
+import path from "path";
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 
@@ -24,6 +29,19 @@ import projectRoute from './routes/projectRoute.js';
 
 app.use('/api/user', userRoutes);
 app.use('/api/user/project', projectRoute);
+
+// Serve static frontend files
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// })
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
